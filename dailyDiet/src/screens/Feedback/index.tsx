02e7e905253db9c74f39375dd81a163490d4ba5a
Bia-source <bia_ferreirads@yahoo.com>
2023-07-08 13:01:12 -0300

    insert loading

--- dailyDiet/src/screens/Feedback/index.tsx
+++ dailyDiet/src/screens/Feedback/index.tsx
@@ -1,10 +1,12 @@
 import { Separator } from '@components/Separator ';
 import * as S from './style';
-import iconGirl from "@assets/girl.png";
-import iconBoy from "@assets/boy.png";
+import iconGirl from "./assets/girl.png";
+import iconBoy from "./assets/boy.png";
 import { Button } from '@components/Button';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Image } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useEffect, useState } from 'react';
 
 type RouteParams = {
     isDiet: boolean;
@@ -14,59 +16,82 @@ export function Feedback() {
     const navigator = useNavigation();
     const route = useRoute();
     const { isDiet } = route.params as RouteParams;
+    const [isLoading, setIsLoanding] = useState<boolean>(true);
 
     function backHome() {
         navigator.navigate('home');
     }
 
+    useEffect(() => {
+        loading();
+    }, []);
+
+    function loading() {
+        setTimeout(() => { setIsLoanding(false) }, 5000)
+    }
+
     return (
         <S.Container>
-            {isDiet ?
+            {isLoading ?
                 <>
-                    <S.Title isDiet>
-                        Continue assim!
-                    </S.Title>
-                    <S.SubTitle> Você continua {""}
-                        <S.Bold>
-                            dentro da dieta.{" "}
-                        </S.Bold>
-                        Muito bem!</S.SubTitle>
+                    <LottieView
+                        autoPlay
+                        loop
+                        style={{ width: 300, height: 300 }}
+                        source={require('../../assets/lottiefile/food-animation.json')}
+                    />
                 </>
-
                 :
                 <>
-                    <S.Title>
-                        Que pena!
-                    </S.Title>
-                    <S.SubTitle> Você {""}
-                        <S.Bold>
-                            saiu da dieta{" "}
-                        </S.Bold>
-                        dessa vez, mas continue se esforndo e não desista!
-                    </S.SubTitle>
 
+                    {isDiet ?
+                        <>
+                            <S.Title isDiet>
+                                Continue assim!
+                            </S.Title>
+                            <S.SubTitle> Você continua {""}
+                                <S.Bold>
+                                    dentro da dieta.{" "}
+                                </S.Bold>
+                                Muito bem!</S.SubTitle>
+                        </>
+
+                        :
+                        <>
+                            <S.Title>
+                                Que pena!
+                            </S.Title>
+                            <S.SubTitle> Você {""}
+                                <S.Bold>
+                                    saiu da dieta{" "}
+                                </S.Bold>
+                                dessa vez, mas continue se esforndo e não desista!
+                            </S.SubTitle>
+
+                        </>
+
+                    }
+
+                    <Separator distance={50} />
+
+                    {isDiet ?
+                        <Image
+                            source={iconGirl}
+                        />
+                        :
+                        <Image
+                            source={iconBoy}
+                        />
+                    }
+                    <Separator distance={30} />
+                    <Button
+                        type='PRIMARY'
+                        size='MD'
+                        text='Ir para a pagina inicial'
+                        onPress={backHome}
+                    />
                 </>
-
             }
-
-            <Separator distance={50} />
-
-            {isDiet ?
-                <Image
-                    source={iconGirl}
-                />
-                :
-                <Image
-                    source={iconBoy}
-                />
-            }
-            <Separator distance={30} />
-            <Button
-                type='PRIMARY'
-                size='MD'
-                text='Ir para a pagina inicial'
-                onPress={backHome}
-            />
         </S.Container>
     )
 }
